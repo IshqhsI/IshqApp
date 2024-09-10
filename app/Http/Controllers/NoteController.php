f71ba75582a11080e11f39f70fb9c9aa65921b0e
IshqhsI <oneridh18@gmail.com>
2024-09-10 23:08:27 +0800

    add note controller

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    public function index()
+    {
+        $notes = Note::all();
+        return view('notes.index', compact('notes'));
+    }
+}
