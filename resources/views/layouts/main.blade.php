db65bc521bb56231d876db7c24226b05cab2f88f
IshqhsI <oneridh18@gmail.com>
2024-09-17 22:35:44 +0800

    update source css & js after build

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <link rel="stylesheet" href="{{ asset('build/assets/app-DxZc1Buv.css') }}">
+    <script src="{{ asset('build/assets/app-DLXkxiZ3.js') }}"></script>
 
     <style>
         @keyframes fade-in-down {
