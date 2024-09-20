7761df15e9d03ae6e01ad21fbb8450a240bbebc0
IshqhsI <oneridh18@gmail.com>
2024-09-20 20:50:17 +0800

    update todo controller: change variabel in method index

--- app/Http/Controllers/ToDoController.php
+++ app/Http/Controllers/ToDoController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\ToDo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ToDoController extends Controller
 {
-    public function index(){
-        $todos = ToDo::all();
-        return view('todos.index', compact('todos'));
+    public function index()
+    {
+        $todos = ToDo::where('user_id', Auth::user()->id)->orderBy('priority', 'desc')->latest()->get();
+        return view(' productivity.todos', compact('todos'));
     }
 }
