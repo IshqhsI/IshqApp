ed4e0487e5023a706b238cd56c6eedb218ac2c91
IshqhsI <oneridh18@gmail.com>
2024-09-26 22:27:39 +0800

    update schedule controller: add method update

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -36,6 +36,27 @@ class ScheduleController extends Controller
         return redirect('/productivity/schedules')->with('success', 'Schedule created successfully.');
     }
 
+    public function update(Request $request){
+        $request->validate([
+            'title' => 'required',
+            'type' => 'required',
+            'description' => 'required',
+            'start_time' => 'required'
+        ]);
+
+        Schedule::where('id', $request->id)->update([
+            'title' => $request->title,
+            'type' => $request->type,
+            'description' => $request->description,
+            'start_time' => $request->start_time,
+            'end_time' => ($request->end_time) ? $request->end_time : null,
+            'is_all_day' => ($request->is_all_day == 'on') ? true : false,
+            'user_id' => Auth::user()->id
+        ]);
+
+        return redirect('/productivity/schedules')->with('success', 'Schedule edited successfully.');
+    }
+
     public function destroy(Schedule $schedule){
         $schedule->delete();
         return back();
